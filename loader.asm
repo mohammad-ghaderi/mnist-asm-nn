2274bcb117f0f1daea44318f6aa45e6c045750c2
mohammad-ghaderi <mohammadunique2002@gmail.com>
2025-09-27 20:07:51 +0330

    loading image by index not just the first image

--- loader.asm
+++ loader.asm
@@ -7,51 +7,59 @@ extern label
 section .text
 
 ; rdi = pointer to image buffer
+; rsi = image index (0-based)
 load_mnist_image:
-    ; open images file
-    mov rax, 2        ; open syscall
-    mov rsi, 0        ; O_RDONLY
+    mov rax, 2        ; open images file
     mov rdi, img_file ; address of filename
-    syscall
+    mov rsi, 0        ; O_RDONLY
+    syscall       
     mov rbx, rax      ; fd
     ; skip header
     mov rax, 8        ; lseek
     mov rdi, rbx
-    mov rsi, 16
+    mov rsi, rsi      ; image index
+    imul rsi, 784     ; image offset
+    add rsi, 16       ; skip 16-byte header
     mov rdx, 0
     syscall
+
     ; read 784 bytes
-    mov rax, 0
+    mov rax, 0  
     mov rdi, rbx
     lea rsi, [rel img]
     mov rdx, 784
     syscall
+
     ; close
-    mov rax, 3
+    mov rax, 3       
     mov rdi, rbx
     syscall
     ret
 
 ; rdi = pointer to label buffer
+; rsi = label index (0-based)
 load_mnist_label:
     ; open labels file
     mov rax, 2
-    mov rsi, 0
     mov rdi, label_file
+    mov rsi, 0
     syscall
     mov rbx, rax
     ; skip header (8 bytes)
     mov rax, 8
     mov rdi, rbx
-    mov rsi, 8
+    mov rsi, rsi      ; label index
+    add rsi, 8        ; skip 8-byte header
     mov rdx, 0
     syscall
+
     ; read 1 byte
     mov rax, 0
     mov rdi, rbx
     lea rsi, [rel label]
     mov rdx, 1
     syscall
+
     ; close
     mov rax, 3
     mov rdi, rbx
