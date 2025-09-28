39f007cb4779eda966310578f72c8e76a4def850
mohammad-ghaderi <mohammadunique2002@gmail.com>
2025-09-28 21:46:08 +0330

    shell file for building the executable file

--- build.sh
+++ build.sh
@@ -6,9 +6,20 @@ mkdir -p obj
 nasm -f elf64 -g -F dwarf _start.asm -o obj/_start.o
 nasm -f elf64 -g -F dwarf loader.asm -o obj/loader.o
 nasm -f elf64 -g -F dwarf mnist_data.asm -o obj/mnist_data.o
-nasm -f elf64 -g -F dwarf print.asm -o obj/print.o
+nasm -f elf64 -g -F dwarf layers_buffer.asm -o obj/layers_buffer.o
+nasm -f elf64 -g -F dwarf layers_data.asm -o obj/layers_data.o
+nasm -f elf64 -g -F dwarf dot_product.asm -o obj/dot_product.o
+nasm -f elf64 -g -F dwarf exp_double.asm -o obj/exp_double.o
+nasm -f elf64 -g -F dwarf neg_log.asm -o obj/neg_log.o
+nasm -f elf64 -g -F dwarf softmax.asm -o obj/softmax.o
+nasm -f elf64 -g -F dwarf reLU_activation.asm -o obj/reLU_activation.o
+nasm -f elf64 -g -F dwarf forward_path.asm -o obj/forward_path.o
 
 # Link
-ld -o mnist obj/_start.o obj/loader.o obj/mnist_data.o obj/print.o
+ld -o mnist obj/exp_double.o obj/softmax.o obj/dot_product.o obj/neg_log.o\
+    obj/loader.o obj/mnist_data.o obj/layers_buffer.o obj/layers_data.o \
+    obj/reLU_activation.o obj/forward_path.o obj/_start.o \
+   -dynamic-linker /lib64/ld-linux-x86-64.so.2 -lc -lm
+
 
 echo "Build complete: ./mnist"
