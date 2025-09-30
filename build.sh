c6b105e396f12487df45084f26a2878dfc31eb56
mohammad-ghaderi <mohammadunique2002@gmail.com>
2025-10-01 01:04:52 +0330

    updating build file

--- build.sh
+++ build.sh
@@ -14,11 +14,15 @@ nasm -f elf64 -g -F dwarf neg_log.asm -o obj/neg_log.o
 nasm -f elf64 -g -F dwarf softmax.asm -o obj/softmax.o
 nasm -f elf64 -g -F dwarf reLU_activation.asm -o obj/reLU_activation.o
 nasm -f elf64 -g -F dwarf forward_path.asm -o obj/forward_path.o
+nasm -f elf64 -g -F dwarf backprop.asm -o obj/backprop.o
+nasm -f elf64 -g -F dwarf matrix_ops.asm -o obj/matrix_ops.o
+nasm -f elf64 -g -F dwarf gradients.asm -o obj/gradient.o
 
 # Link
 ld -o mnist obj/exp_double.o obj/softmax.o obj/dot_product.o obj/neg_log.o\
     obj/loader.o obj/mnist_data.o obj/layers_buffer.o obj/layers_data.o \
-    obj/reLU_activation.o obj/forward_path.o obj/_start.o \
+    obj/reLU_activation.o obj/forward_path.o obj/gradient.o\
+    obj/matrix_ops.o obj/backprop.o obj/_start.o \
    -dynamic-linker /lib64/ld-linux-x86-64.so.2 -lc -lm
 
 
