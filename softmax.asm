1c150991c943a4ba9d4bd2ef3236fb29bc302fe8
mohammad-ghaderi <mohammadunique2002@gmail.com>
2025-10-07 21:07:01 +0330

    add numberical stablity to softmax implementaion

--- softmax.asm
+++ softmax.asm
@@ -1,5 +1,5 @@
 global softmax
-extern exp_double   ; you need to implement this separately
+extern exp_double
 
 section .text
 
@@ -9,28 +9,42 @@ section .text
 ; rcx = length
 softmax:
     push rbx
+    push r12
+    mov r12, rcx          ; save length
+    
+    ;Find maximum value
+    movsd xmm7, [rdi]     ; max = input[0]
     xor rbx, rbx
-    pxor xmm7, xmm7       ; sum = 0.0
-
-.loop_exp:
-    movsd xmm0, [rdi + rbx*8]   ; load input[i]
-    call exp_double            ; xmm0 = exp(input[i])
-    movsd [rsi + rbx*8], xmm0  ; store temp result
-    addsd xmm7, xmm0           ; accumulate sum
+.find_max_loop:
+    movsd xmm0, [rdi + rbx*8]
+    maxsd xmm7, xmm0      ; xmm7 = max(xmm7, xmm0)
     inc rbx
     cmp rbx, rcx
+    jl .find_max_loop
+    
+    ; Compute exp(input[i] - max) and sum
+    pxor xmm6, xmm6       ; sum = 0.0
+    xor rbx, rbx
+.loop_exp:
+    movsd xmm0, [rdi + rbx*8]
+    subsd xmm0, xmm7      ; xmm0 = input[i] - max
+    call exp_double       ; xmm0 = exp(input[i] - max)
+    movsd [rsi + rbx*8], xmm0
+    addsd xmm6, xmm0      ; accumulate sum
+    inc rbx
+    cmp rbx, r12
     jl .loop_exp
-
-    ; normalize: output[i] /= sum
-    mov rdx, rbx               ; rdx = length
+    
+    ; Normalize
     xor rbx, rbx
 .normalize_loop:
     movsd xmm0, [rsi + rbx*8]
-    divsd xmm0, xmm7
+    divsd xmm0, xmm6      ; xmm0 /= sum
     movsd [rsi + rbx*8], xmm0
     inc rbx
-    cmp rbx, rdx
+    cmp rbx, r12
     jl .normalize_loop
-
+    
+    pop r12
     pop rbx
     ret
