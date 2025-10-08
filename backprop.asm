41a6697700a4420891c09d32ddde4a5baae58eaf
mohammad-ghaderi <mohammadunique2002@gmail.com>
2025-10-08 20:23:55 +0330

    using h1 and h2 insted of z1 and z2, changing matrix_vector_multiply_transpose to matrix_vector_multiply

--- backprop.asm
+++ backprop.asm
@@ -3,7 +3,7 @@ extern img_double, label, h1, h2, o, z1, z2
 extern W1, W2, W3
 extern dW1, dbias1, dW2, dbias2, dW3, dbias3
 extern grad_h1, grad_h2, grad_o
-extern outer_product_add, matrix_vector_multiply_transpose
+extern outer_product_add, matrix_vector_multiply
 
 section .data
 one dq 1.0
@@ -25,9 +25,9 @@ accumulate_gradients:
     call softmax_cross_entropy_backward
 
     ; Layer 3 gradients (W3, b3)
-    ; dW3 += h2^T * grad_o
-    lea rdi, [rel h2]         ; input to layer 3
-    lea rsi, [rel grad_o]     ; gradient from output
+    ; dW3 += grad_o^T * h2
+    lea rdi, [rel grad_o]     ; gradient from output
+    lea rsi, [rel h2]         ; input to layer 3
     lea rdx, [rel dW3]        ; gradient for W3
     mov rcx, 10               ; output size
     mov r9, 64                ; input size
@@ -44,25 +44,23 @@ accumulate_gradients:
     cmp rax, rcx
     jl .accumulate_db3_loop
 
-    ; grad_h2 = W3^T * grad_o
+    ; grad_h2 = grad_o * W3
     lea rdi, [rel grad_o]     ; gradient from output
     lea rsi, [rel W3]         ; weights
     lea rdx, [rel grad_h2]    ; gradient for h2
-    mov rcx, 64               ; output size
-    mov r9, 10                ; input size
-    call matrix_vector_multiply_transpose
+    mov rcx, 10               ; grad_o size
+    mov r9, 64                ; W3 columns
+    call matrix_vector_multiply
 
     ; Layer 2 gradients with ReLU
-    ; grad_z2 = grad_h2 * relu_derivative(z2)
-    lea rdi, [rel z2]         ; pre-activation
+    lea rdi, [rel h2]         ; pre-activation
     lea rsi, [rel grad_h2]    ; gradient from above
     mov rcx, 64               ; size
     call relu_backward         ; result in grad_h2 (now it's grad_z2)
-    ; we store grade_z2 in grade_h2 for memory efficiency, because we don't need grade_h2 anymore
 
-    ; dW2 += h1^T * grad_z2
-    lea rdi, [rel h1]         ; input to layer 2
-    lea rsi, [rel grad_h2]    ; gradient (now grad_z2)
+    ; dW2 += grad_z2^T * h1
+    lea rdi, [rel grad_h2]    ; gradient (grad_z2) (size 64)
+    lea rsi, [rel h1]         ; input to layer 2 (size 128)
     lea rdx, [rel dW2]        ; gradient for W2
     mov rcx, 64               ; output size
     mov r9, 128               ; input size
@@ -79,28 +77,26 @@ accumulate_gradients:
     cmp rax, rcx
     jl .accumulate_db2_loop
 
-    ; grad_h1 = W2^T * grad_z2
+    ; grad_h1 = grad_z2 * W2
     lea rdi, [rel grad_h2]    ; gradient (grad_z2)
     lea rsi, [rel W2]         ; weights
     lea rdx, [rel grad_h1]    ; gradient for h1
-    mov rcx, 128              ; output size
-    mov r9, 64                ; input size
-    call matrix_vector_multiply_transpose
+    mov rcx, 64               ; grad_z2 size
+    mov r9, 128               ; W2 columns
+    call matrix_vector_multiply
 
     ; Layer 1 gradients with ReLU
-    ; grad_z1 = grad_h1 * relu_derivative(z1)
-    lea rdi, [rel z1]         ; pre-activation
+    lea rdi, [rel h1]         ; pre-activation
     lea rsi, [rel grad_h1]    ; gradient from above
     mov rcx, 128              ; size
     call relu_backward         ; result in grad_h1 (now it's grad_z1)
-    ; we store grade_z1 in grade_h1 for memory efficiency, because we don't need grade_h1 anymore
 
-    ; dW1 += input^T * grad_z1 
-    lea rdi, [rel img_double]        ; input image
-    lea rsi, [rel grad_h1]    ; gradient (now grad_z1)
+    ; dW1 += grad_z1^T * img
+    lea rdi, [rel grad_h1]    ; gradient (grad_z1)
+    lea rsi, [rel img_double] ; input image (size 784)
     lea rdx, [rel dW1]        ; gradient for W1
-    mov rcx, 128              ; output size
-    mov r9, 784               ; input size
+    mov r9, 128               ; size of grad_z1
+    mov rcx, 784              ; size of img
     call outer_product_add
 
     ; dbias1 += grad_z1
@@ -119,7 +115,6 @@ accumulate_gradients:
 
 relu_backward:
     ; rdi = pre-activation z, rsi = gradient from above, rcx = size
-    ; computes gradient through ReLU: grad_z = grad_h * relu_derivative(z)
     push rbp
     mov rbp, rsp
     xor rax, rax
@@ -128,14 +123,12 @@ relu_backward:
     xorpd xmm1, xmm1
     comisd xmm0, xmm1
     jbe .zero_grad
-    ; z > 0: grad_z = grad_h (pass through)
     movsd xmm0, [rsi + rax*8]  ; gradient from above
     jmp .store_grad
 .zero_grad:
-    ; z <= 0: grad_z = 0
     xorpd xmm0, xmm0
 .store_grad:
-    movsd [rsi + rax*8], xmm0  ; store back to gradient array
+    movsd [rsi + rax*8], xmm0
     inc rax
     cmp rax, rcx
     jl .relu_backward_loop
@@ -154,16 +147,13 @@ softmax_cross_entropy_backward:
     movsd xmm0, [rdi + r8*8]   ; p_i
     cmp r8, rax
     jne .not_true_class
-    ; For true class: grad = p_i - 1
     subsd xmm0, [rel one]
     jmp .store_grad
 .not_true_class:
-    ; For other classes: grad = p_i
 .store_grad:
     movsd [rdx + r8*8], xmm0
     inc r8
     cmp r8, rcx
     jl .softmax_grad_loop
-    
     pop rbp
     ret
