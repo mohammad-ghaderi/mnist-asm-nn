7de35f0406137d84e9c230303b7eec6373df58b2
mohammad-ghaderi <mohammadunique2002@gmail.com>
2025-10-28 21:54:40 +0330

    switching from double to float32

--- argmax.asm
+++ argmax.asm
@@ -2,19 +2,19 @@ global argmax
 
 section .text
 ; argmax(array, length)
-; rdi = address of array (double[])
+; rdi = address of array (float[])
 ; rcx = number of elements
 ; returns: rax = index of maximum value
 argmax:
     xor rax, rax        ; rax = best index
     xor rbx, rbx        ; rbx = loop index
-    movsd xmm1, [rdi]   ; xmm1 = current max value
+    movss xmm1, [rdi]   ; xmm1 = current max value
 
 .loop:
-    movsd xmm0, [rdi + rbx*8]
-    ucomisd xmm0, xmm1
+    movss xmm0, [rdi + rbx*4]
+    ucomiss xmm0, xmm1
     jbe .skip_update
-    movsd xmm1, xmm0
+    movss xmm1, xmm0
     mov rax, rbx        ; save new max index
 .skip_update:
     inc rbx
